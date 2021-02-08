2f06a0e15baa40ff7e8dbc976793c631c8c5bb9b
takakun <takajcb@gmail.com>
2021-02-09 00:00:47 +0900

    comment on sh file

--- deploy_prod_branch.sh
+++ deploy_prod_branch.sh
@@ -2,3 +2,14 @@ heroku login
 git add -A
 git commit -m "deploy"
 git push heroku production:main
+
+
+# heroku run flask db migrate
+# heroku run flask db upgrade
+
+# heroku run python
+# from Hearvo.models import Lang, db, VoteType
+# db.create_all()
+
+# heroku run python3 db_init_for_heroku.py
+
