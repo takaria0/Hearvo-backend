e75857efb68487097f9bf9c3d69843e83013ee1e
takakun <takajcb@gmail.com>
2021-02-08 23:34:06 +0900

    migration

--- entrypoint.local.sh
+++ entrypoint.local.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
  
-# echo flask db init
-# flask db init
+echo flask db init
+flask db init
 
 echo flask db migrate
 flask db migrate
@@ -10,6 +10,7 @@ echo flask db upgrade
 flask db upgrade
 
 # echo init database
+# docker exec -it XXX bash
 # python3 db_init_for_heroku.py
 
 echo python3 run.py
