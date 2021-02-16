19fb2ee10117a589bab38da51a0255057bf25f87
takakun <takajcb@gmail.com>
2021-02-16 21:15:59 +0900

    temp delete try except

--- Hearvo/apis/posts.py
+++ Hearvo/apis/posts.py
@@ -2,6 +2,8 @@ import os
 from collections import Counter
 from datetime import datetime, timedelta, timezone
 import json
+import traceback
+      
 
 from flask import request, Response, abort, jsonify, Blueprint
 from flask_restful import Resource
@@ -834,29 +836,29 @@ class PostResource(Resource):
         created_at=datetime.now(timezone(timedelta(hours=0), 'UTC')).isoformat()
       )
 
-      try:
-        db.session.add(new_post)
-        db.session.flush()
+      # try:
+      db.session.add(new_post)
+      db.session.flush()
 
-        post_id = new_post.id
+      post_id = new_post.id
 
-        """ 
-        if this was posted in a group, update a relation between the post and the group
-        """
-        update_num_of_posts(group_id, post_id)
+      """ 
+      if this was posted in a group, update a relation between the post and the group
+      """
+      update_num_of_posts(group_id, post_id)
 
-        """
-        save topics of the posts
-        """
-        topic_ids = save_unique_topic(topic_list, country_id, post_id, group_id)
+      """
+      save topics of the posts
+      """
+      topic_ids = save_unique_topic(topic_list, country_id, post_id, group_id)
 
-        db.session.commit()
-        status_code = 200
-        return post_schema.dump(new_post), status_code
+      db.session.commit()
+      status_code = 200
+      return post_schema.dump(new_post), status_code
 
-      except:
-        db.session.rollback()
-        return {}, 400
+      # except:
+      #   db.session.rollback()
+      #   return {}, 400
 
     """
     vote_type_id = 2. this post is majority judgement
